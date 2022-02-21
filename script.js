b9dcb082dd14f0adfc427ca9867b78bb40945868
paksisoma <somx99@gmail.com>
2022-02-21 12:52:27 +0100

    Számítások

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-function szamitas() {
+const nev = document.getElementById("nev");
+const lakcim = document.getElementById("lakcim");
+const szuletes = document.getElementById("szuletes");
+const gepjarmu = document.getElementById("gepjarmu");
+const uzemanyag = document.getElementById("uzemanyag");
+const fogyasztas = document.getElementById("fogyasztas");
+const utazas = document.getElementById("utazas");
+const egysegar = document.getElementById("egysegar");
+const kiindulas = document.getElementById("kiindulas");
+const uticel = document.getElementById("uticel");
+const tovabbiuticel = document.getElementById("tovabbiuticel");
+const megtett = document.getElementById("megtett");
 
+const utikoltseg = document.getElementById("utikoltseg");
+const amortizacio = document.getElementById("amortizacio");
+const osszeskoltseg = document.getElementById("osszeskoltseg");
+
+function szamitas() {
+	/*if (!nev.value || !lakcim.value || !szuletes.value || !gepjarmu.value) {
+		console.log("asd");
+	}*/
+
+	const a = parseInt(fogyasztas.value) * egysegar.value * megtett.value / 100;
+	const b = megtett.value * 15;
+
+	utikoltseg.innerHTML = a + " Ft";
+	amortizacio.innerHTML = b + " Ft";
+	osszeskoltseg.innerHTML = a + b + " Ft";
 }
 
 function nyomtatas() {
-	
+
 }
