6a899b5c1fc5c4dac6a47d79891770fe2aa1f564
paksisoma <somx99@gmail.com>
2022-02-21 13:30:58 +0100

    Nyomtatás

--- script.js
+++ script.js
@@ -15,30 +15,33 @@ const utikoltseg = document.getElementById("utikoltseg");
 const amortizacio = document.getElementById("amortizacio");
 const osszeskoltseg = document.getElementById("osszeskoltseg");
 
-let szamit = document.getElementById("aszamitas");
 let nyomtat = document.getElementById("anyomtatas");
 
-let forras = document.getElementById("forras");
+let datum = document.getElementById("datum");
+datum.innerHTML = most()
+
+function most() {
+	let today = new Date();
+	let dd = String(today.getDate()).padStart(2, '0');
+	let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+	let yyyy = today.getFullYear();
+
+	return yyyy + ". " + mm + ". " + dd + ".";
+}
 
 function szamitas() {
-	/*if (!nev.value || !lakcim.value || !szuletes.value || !gepjarmu.value) {
-		console.log("asd");
-	}*/
-
-	const a = parseInt(fogyasztas.value) * egysegar.value * megtett.value / 100;
-	const b = megtett.value * 15;
-
-	utikoltseg.innerHTML = a + " Ft";
-	amortizacio.innerHTML = b + " Ft";
-	osszeskoltseg.innerHTML = a + b + " Ft";
-
-	nyomtat.disabled = false;
+	//if (nev.value && lakcim.value && szuletes.value && gepjarmu.value && uzemanyag.value && fogyasztas.value && utazas.value && egysegar.value && kiindulas.value && uticel.value && tovabbiuticel.value && megtett.value) {
+		const a = parseInt(fogyasztas.value) * egysegar.value * megtett.value / 100;
+		const b = megtett.value * 15;
+	
+		utikoltseg.innerHTML = a + " Ft";
+		amortizacio.innerHTML = b + " Ft";
+		osszeskoltseg.innerHTML = a + b + " Ft";
+	
+		nyomtat.disabled = false;
+	//}
 }
 
 function nyomtatas() {
-	/*szamit.style.display = "none";
-	nyomtat.style.display = "none";
-	forras.style.display = "none";*/
-
-	/*window.print()*/
+	window.print()
 }
